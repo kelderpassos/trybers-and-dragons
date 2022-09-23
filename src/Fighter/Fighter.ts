3e7d450aa6a70235c430b217f88da756d35779c5
Kelder Passos <kelder.passos@gmail.com>
2022-09-23 17:33:38 -0300

    feat: adc req09 pronto

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -2,8 +2,8 @@ import Energy from '../Energy';
 import SimpleFighter from './SimpleFighter';
 
 export default interface Fighter extends SimpleFighter {
-  defense: number;
+  defense?: number;
   energy?: Energy;
   special?(enemy: Fighter): void;
-  levelUp(): void;
+  levelUp?(): void;
 }
